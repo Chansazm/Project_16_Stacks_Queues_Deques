10793a7f146476f56c62e8a37c15a2bb42f554a9
Chansa Chishimba <chansazm@yahoo.co.uk>
2020-03-11 10:19:40 -0500

    added tests

--- Stacks.py
+++ Stacks.py
@@ -1,6 +1,3 @@
-class Empty(Exception):
-    """Error attempting to access from an empty container."""
-    pass
 
 class ArrayStack:
     """LIFO stack implementation using a python list"""
@@ -9,50 +6,41 @@ class ArrayStack:
         """create an empty stack"""
         self.data = []
 
-    def __len__(self):
-        """ return the number of elements in the stack"""
-        return len(self.data)
-    
-    def is_empty(self):
-        """return true if stack is empty"""
-        return len(self.data) == 0:
+    def push(self,item):
+        """Add an element to the end of the stack and do not return anything,time O(1)"""
+        self.data.append(item)  
 
-    def push(self,e):
-        """Add an element to the end of the stack"""
-        self.data.append(e)
-    
-    def top(self):
-        """return the element at the top but do not remove"""
-        if self.data is_empty():
-            raise Empty("Stack is empty")
-        return self.data[-1]
-    
     def pop(self):
         """remove an element from the top of the stack"""
         if is_empty(self):
             raise Exception("Stack is empty")
         return self.data.pop()
 
-
-
-
-    def reverse_file(filename):
-        S = ArrayStack()
-        original = open(filename)
-        for line in original:
-        S.push(line.rstrip('\n')
-        original.close()
-
-
-        """Overwrite with contents in LIFO order"""
-        """reopen the file and write"""
-        output = open(filename,'w')
-        while not S.is_empty():
-
-            output.write(S.pop() + '\n')
-            output.close()
-
-
+    def __len__(self):
+        """ return the number of elements in the stack"""
+        return len(self.data)
     
+    def is_empty(self):
+        """return true if stack is empty"""
+        return len(self.data) == 0
+
+    def top(self):
+        """return the element at the top but do not remove"""
+        if self.is_empty():
+            raise Empty("Stack is empty")
+        return self.data[-1]
+
+#********************************************************************
+#Tests
+
+Stack = ArrayStack()
+Stack.push(9)
+Stack.push(2)
+Stack.top()
+
+Stack = [5,4,1,8, 2,8,9,7]
+#Stack.sort()
+temp = Stack[2]
+print(temp)
 
 
--- queue.py
+++ queue.py
@@ -1,6 +1,48 @@
+class queue:
     def __init__(self):
         """create an empty stack"""
-        self.data = []
+        self.items = []
 
-    def enqueue(self,item):
-        return self.data.insert(0,item)
+    def enqueue(self, item):
+        return self.items.insert(0, item)
+        """add an item at the end of the queue, takes O(n)
+        """
+    # O(n)
+
+    def dequeue(self):
+        self.items.pop()
+        """remove an item from the begining of the queue in O(1)
+        """
+    # O(1)
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        """return but do not remove the next item to be removed
+        """
+
+    def size(self):
+        return len(self.items)
+        """[summary]
+        """
+
+    def is_empty(self):
+        return self.items == 0
+
+    # todo: tests
+    # *great work
+    # ? need to do more
+
+    # ***********************************************************
+    # Tests
+
+
+myq = queue()
+myq.enqueue('8')
+myq.enqueue('7')
+myq.enqueue('6')
+myq.enqueue('5')
+myq.enqueue('4')
+print(myq.peek())
+print(myq.size())
+print(myq.is_empty())
